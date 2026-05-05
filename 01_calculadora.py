206dfb77f9bb08d0d24f3070bee2ef15d70a65f8
Melody Carbone <carbonemelody@gmail.com>
2026-05-05 16:47:20 -0300

    Create 01_calculadora.py
    Calculadora basica

---
+++ 01_calculadora.py
@@ -0,0 +1,23 @@
+numero1=float(input("Ingrese un numero: "))
+accion=input("Ingrese la accion a realizar: ")
+numero2=float(input("Ingrese otro numero: "))
+if accion == "/":
+    if numero2 != 0:
+        resultado=numero1 / numero2
+        print ("El resultado es:",resultado)
+    else:
+        print("No se puede dividir un numero por 0.")
+
+elif accion == "+":
+    resultado=numero1 + numero2
+    print("El resultado es:",resultado)
+elif accion == "-":
+    resultado= numero1 - numero2
+    print("El resultado es:", resultado)
+elif accion == "*":
+    resultado = numero1 * numero2
+    print("El resultado es:", resultado)
+
+
+else:
+    print("La accion no es valida. Por favor ingrese: *,+,- o /.") 
